95bb16034280ef707e6f1854fc2776d624298b20
ChubbyCub <elise.pham88@gmail.com>
2021-01-25 16:41:38 -0800

    Dice Roll Random Basic

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,11 +1,28 @@
-package com.example.diceroller
+ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.Random
 
+ // use this AppCompatActivity to make the app more compatible with more devices.
 class MainActivity : AppCompatActivity() {
+     // always override
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+         val rollButton = findViewById<Button>(R.id.roll_button)
+         rollButton.text = getString(R.string.LetsRoll)
+         rollButton.setOnClickListener {
+             rollDice()
+         }
     }
-}
+
+     private fun rollDice() {
+         val resultText: TextView = findViewById<TextView>(R.id.result_text)
+         val randomInt = Random().nextInt(6) + 1
+         resultText.text = randomInt.toString()
+     }
+ }
