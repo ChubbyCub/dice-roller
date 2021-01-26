ac7acd3d4a85e0697438281bf690a029d2858767
ChubbyCub <elise.pham88@gmail.com>
2021-01-25 20:54:32 -0800

    Added dynamic images

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,21 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 import java.util.Random
 
  // use this AppCompatActivity to make the app more compatible with more devices.
 class MainActivity : AppCompatActivity() {
-     // always override
+     // so this lateinit will make it so that we do not need to set the variable to null
+     // at the beginning.
+     lateinit var diceImage: ImageView
+     // always override this abstract method from AppCompatActivity
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+         super.onCreate(savedInstanceState)
+         setContentView(R.layout.activity_main)
          val rollButton = findViewById<Button>(R.id.roll_button)
          rollButton.text = getString(R.string.LetsRoll)
          rollButton.setOnClickListener {
              rollDice()
          }
+         diceImage = findViewById<ImageView>(R.id.dice_image)
     }
 
      private fun rollDice() {
@@ -30,7 +32,10 @@ class MainActivity : AppCompatActivity() {
              5 -> R.drawable.dice_5
              else -> R.drawable.dice_6
          }
-         val diceImage: ImageView = findViewById<ImageView>(R.id.dice_image)
+         // we want to minimize the number of times we call findViewById because this requires
+         // the app to search the view hierarchy for the correct view, which takes time.
+
+         // so best practice is to keep the image in a field, which is a class variable here.
          diceImage.setImageResource(drawableResource)
      }
  }
